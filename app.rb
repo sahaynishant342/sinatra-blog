a46aa7d346b64c6e8be39da11ff2e021ec1bc80f
Nishant Kumar <nishant.address@gmail.com>
2017-11-29 18:33:10 +0530

    added functionality to create and view posts

--- app.rb
+++ app.rb
@@ -7,3 +7,40 @@ require './environments'
 
 class Post < ActiveRecord::Base
 end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    erb :"posts/create"
+  end
+end
+
+get "/posts/:id" do
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome."
+    end
+  end
+end
